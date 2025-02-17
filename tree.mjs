4e32e81cd700dc07a725db5db697b30c7c073b42
Lancer385 <ahmedkhalaf1234123@gmail.com>
2025-02-17 13:48:50 +0200

    implement mergeSort for sorting provided input and a method to remove duplicates

--- tree.mjs
+++ tree.mjs
@@ -1,6 +1,6 @@
 import { Node } from "./node.mjs";
 
-class Tree{
+export class Tree{
   constructor(){
     this.root = null;
   }
@@ -12,7 +12,11 @@ class Tree{
     root.right = this.buildTree(arr, middle + 1, end);
     this.root = root;
     return root;
-  }
+  };
+  build(array){
+    const newArray = [...new Set(this.mergeSort(array))]
+    return this.buildTree(newArray, 0, newArray.length - 1)
+  };
   prettyPrint(node, prefix = "", isLeft = true){
     if (node === null) {
       return;
@@ -24,5 +28,35 @@ class Tree{
     if (node.left !== null) {
       this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
     }
+  };
+  mergeSort(array, mergedArray = []){
+    if (array.length <= 1) {
+        return array;
+    };
+    const midPoint = Math.ceil(array.length / 2);
+    const leftArray = this.mergeSort(array.slice(0, midPoint));
+    const rightArray = this.mergeSort(array.slice(midPoint, array.length));
+    while (leftArray.length > 0 || rightArray.length > 0){
+        if (leftArray.length === 0){
+            mergedArray.push(...rightArray);
+            break;
+        }
+        else if (rightArray.length === 0){
+            mergedArray.push(...leftArray);
+            break;
+        };
+        if (leftArray[0] < rightArray[0]) {
+            mergedArray.push(leftArray[0]);
+            leftArray.shift();
+        }
+        else if (leftArray[0] > rightArray[0]){
+            mergedArray.push(rightArray[0]);
+            rightArray.shift();
+        }
+        else if (leftArray[0] === rightArray[0]){
+          leftArray.shift();
+        };
+    }
+    return mergedArray;
   };
 }
